e2a36f085aafdb2fcd65c54515b6d9f7a6d96c64
natanngl03 <roy.natanaelngl03@gmail.com>
2026-03-17 08:03:09 +0700

    "first commit"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [react(), icon(), sitemap()],
+
+    site: "https://websitekamu.com",
+});
